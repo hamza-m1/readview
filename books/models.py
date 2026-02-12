db916cdfc43051301aa3f75ff449ba31b1890bc3
hamza-m1 <177459013+hamza-m1@users.noreply.github.com>
2026-02-12 17:45:21 +0000

    Add Book and Genre models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+class Book(models.Model):
+    title = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    author = models.CharField(max_length=100)
+    genre = models.ManyToManyField('Genre', related_name='books')
+    publication_date = models.DateField()
+    length = models.PositiveIntegerField(help_text="Length in pages")
+    isbn = models.CharField(max_length=13, unique=True)
+    summary = models.TextField()
+    posted_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    approved = models.BooleanField(default=False)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-posted_on']
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(unique=True)
+    description = models.CharField(max_length=250)
+
+    def __str__(self):
+        return f"{self.name}"
