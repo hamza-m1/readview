e701f76669cde18b979e0c8dc2ba8e6e8c558509
hamza-m1 <177459013+hamza-m1@users.noreply.github.com>
2026-02-16 16:26:44 +0000

    Create view and template for the list of books

--- books/views.py
+++ books/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello, world!")
+
+def book_list(request):
+    """
+    returns all published books in :model: 'books.Book' and displays them in a 
+    page of 9 books.
+
+    **Context**
+
+    ``books``
+        A queryset of all published books in the database.
+
+    **Template**
+
+    :template:`books/book_list.html`
+    """
+
+    books = Book.objects.filter(status=1)
+
+    context = {
+        'books': books
+    }
+    return render(request, 'books/book_list.html', context)
