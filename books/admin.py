c42666ee3b0da0fae03982ccde192cb015caaa60
hamza-m1 <177459013+hamza-m1@users.noreply.github.com>
2026-02-16 16:48:58 +0000

    Add Summernote integration for Book model

--- books/admin.py
+++ books/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from .models import Book, Genre, Review
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Book)
+class BookAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'author', 'publication_date', 'status')
+    list_filter = ('status', 'publication_date')
+    search_fields = ('title', 'author')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('summary',)
+
 
 # Register your models here.
 admin.site.register(Genre)
-admin.site.register(Book)
 admin.site.register(Review)
