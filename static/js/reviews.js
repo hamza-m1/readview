6cf1a35f0f7e1047f78cf0d826673b0e0220533c
hamza-m1 <177459013+hamza-m1@users.noreply.github.com>
2026-02-24 14:51:33 +0000

    Add review rating retrieval in edit functionality

--- static/js/reviews.js
+++ static/js/reviews.js
@@ -1,8 +1,8 @@
 const editButtons = document.getElementsByClassName("btn-edit");
 const reviewText = document.getElementById("id_content");
-// const reviewRating = document.getElementById("id_rating");
 const reviewForm = document.getElementById("reviewForm");
 const submitButton = document.getElementById("submitButton");
+const reviewRating = document.getElementById("id_rating");
 
 const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
 const deleteButtons = document.getElementsByClassName("btn-delete");
@@ -10,20 +10,15 @@ const deleteConfirm = document.getElementById("deleteConfirm");
 
 for (let button of editButtons) {
   button.addEventListener("click", (e) => {
-    console.log("in the edit button event listener");
     let reviewId = e.target.getAttribute("review_id");
-    // let reviewRating = document
-    //   .getElementById(`reviewRating${reviewId}`)
-    //   .getAttribute("rating");
-    // console.log(reviewRating);
-    console.log(document.getElementById(`reviewContent${reviewId}`));
     let reviewContent = document
       .getElementById(`reviewContent${reviewId}`)
       .innerText.trim();
-    // console.log(reviewContent);
-    // reviewRating.value = reviewRating;
-    // console.log(`reviewRating: ${reviewRating}`);
     reviewText.value = reviewContent;
+    let reviewRatingNum = document
+      .getElementById(`reviewRating${reviewId}`)
+      .getAttribute("rating");
+    reviewRating.value = reviewRatingNum;
     submitButton.innerText = "Update";
     reviewForm.setAttribute("action", `edit_review/${reviewId}`);
   });
